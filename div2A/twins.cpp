baa75932638ea4fcc6d0f5821b18282ca53f4d76
root <root@LAPTOP-IKJTQ2K2.localdomain>
2020-07-17 18:26:04 +0530

    init

---
+++ div2A/twins.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("O2") 
+#include<bits/stdc++.h>
+using namespace std;
+
+{
+    int n;
+    long long int sum=0,coins=0;
+    cin>>n;
+    int xoxo[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>xoxo[i];
+        sum=sum+xoxo[i];
+    }
+    sort(xoxo,xoxo+n,greater<int>());
+    long long int hehe=0,left=sum;
+    for(int i=0;i<n;i++)
+    {
+        hehe=hehe+xoxo[i];
+        coins++;
+        left=sum-hehe;
+        if(hehe>left)
+        {
+            break;
+        }
+    }
+    cout<<coins;
+    return 0;
+}
