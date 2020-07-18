8d90caf022f4d9105634b83d194dcbc1de232451
root <root@LAPTOP-IKJTQ2K2.localdomain>
2020-07-18 11:48:20 +0530

    intial

---
+++ div2A/luckydivision1.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("O2") 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{    
+    int n,count=0,count1=0;
+    cin>>n;
+    int c=n;
+
+    while(n>0)
+    {
+       int a=n%10;
+       count1++;
+       if(a==4 || a==7) { 
+          count++; 
+          cout<<"*"<<endl; 
+          }
+       else{
+          break;
+       }
+       n=n/10;
+       
+    }
+
+    if(count==count1){
+        cout<<"YES";
+        cout<<"*1"<<endl;
+    }
+    else if(c%4==0)
+    {
+        cout<<"YES";
+        cout<<"**"<<endl;
+    }
+    else if(c%2==0 && c%7==0){
+        cout<<"yes";
+        cout<<"***"<<endl;
+    }
+    else{
+        cout<<"NO";
+        cout<<"*0"<<endl;
+    }
+
+    return 0;
+}
